8d8b870094ce1e2c95a75c9e32c9d1dcd66bb443
Pedro Gómez <pedrogomezzambra182@gmail.com>
2025-05-15 19:16:35 -0400

    Incorpore y reorganize el codigo del html antiguo al nuevo estilo de jsx, con su respectiva carpeta module.css, solamente el header y el hero.

---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+// src/pages/Home.jsx
+import React from "react";
+import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
+
+const Home = () => {
+    return (
+        <>
+        <Header />
+        <Hero />
+        </>
+    );
+};
+
+export default Home;
