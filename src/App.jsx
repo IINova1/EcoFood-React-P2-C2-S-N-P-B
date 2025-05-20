13a4b5e7b7828f3597b9f866beafad564b9357cb
Pedro Gómez <pedrogomezzambra182@gmail.com>
2025-05-20 13:14:29 -0400

    todo ordenadito

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import Home from './pages/home';
+import Home from './pages/home'; // Usa rutas relativas, bien escritas y con mayúsculas si el archivo se llama asíimport './App.css';
+import './App.css';
+import './index.css';
+
+
 
 function App() {
   return (
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,28 @@
-// src/pages/Home.jsx
-import React from "react";
-import Header from "../components/Header/Header";
-import Hero from "../components/Hero/Hero";
+import Hero from '../components/Hero';
+import Header from '../components/Header';
+import Somos from '../components/Somos'
+import QueHacemos from '../components/Que_hacemos';
 
-const Home = () => {
+import { getUserData } from "../services/userService";
+import { useAuth } from "../context/AuthContext";
+useEffect(() => {
+const fetch = async () => {
+const datos = await getUserData(user.uid);
+setUserData(datos);
+};
+if (user) fetch();
+}, [user]);
+
+
+function Home() {
     return (
         <>
-        <Header />
-        <Hero />
+        <Header/>
+        <Hero/>
+        <Somos/>
+        <QueHacemos/>
         </>
     );
-};
+}
 
 export default Home;
