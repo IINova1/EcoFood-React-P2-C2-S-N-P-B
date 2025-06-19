a51e7b86e6cda18e313236b053ab35fb00f33e91
Pedro Gómez <pedrogomezzambra182@gmail.com>
2025-06-19 18:08:42 -0400

    tuve problemas con el guardado, ahora otro de cliente luego de iniciar sesion

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from "../routes/ProtectedRoute";
 
 // Layout
-import AdminLayout from '../components/admin/layout/AdminLayout';
+import Layout from "../layout/Layout";
 
-// Contexto y páginas públicas
-import Home from '../pages/home';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import RegistroEmpresa from '../pages/RegistroEmpresa';
-import RegistroAdmin from '../pages/RegistroAdmin';
-import Recuperar from '../pages/Recuperar';
+// Páginas públicas
+import Inicio from "../pages/Inicio";
+import Login from "../pages/Login";
+import RegistroCliente from "../pages/RegistroCliente";
+import RegistroEmpresa from "../pages/RegistroEmpresa";
+import RegistroAdmin from "../pages/RegistroAdmin";
+import Recuperar from "../pages/Recuperar";
 
 // Páginas admin
 import DashboardAdmin from '../pages/admin/DashboardAdmin';
@@ -20,46 +20,84 @@ import AdministradoresAdmin from '../pages/admin/AdministradoresAdmin';
 import Administracion from '../pages/Administracion';
 import ListaClientes from "../pages/admin/clientes/ListaClientes";
 import FormularioCliente from "../pages/admin/clientes/FormularioCliente";
+import ClienteDashboard from '../pages/admin/clientes/ClienteDashboard'; // ✅ Ruta corregida
 
-// ✅ Nuevas páginas empresa
+// Páginas empresa
 import PerfilEmpresa from '../pages/empresa/PerfilEmpresa';
 import ProductosEmpresa from '../pages/empresa/ProductosEmpresa';
 
 export default function AppRouter() {
   return (
     <Routes>
-      {/* Rutas públicas */}
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/registro" element={<Register />} />
-      <Route path="/registro/empresa" element={<RegistroEmpresa />} />
-      <Route path="/registro/admin" element={<RegistroAdmin />} />
-      <Route path="/recuperar" element={<Recuperar />} />
+      <Route path="/" element={<Layout />}>
+        {/* Rutas públicas */}
+        <Route index element={<Inicio />} />
+        <Route path="login" element={<Login />} />
+        <Route path="registro/cliente" element={<RegistroCliente />} />
+        <Route path="registro/empresa" element={<RegistroEmpresa />} />
+        <Route path="registro/admin" element={<RegistroAdmin />} />
+        <Route path="recuperar" element={<Recuperar />} />
 
-      {/* Rutas protegidas EMPRESA */}
-      <Route path="/empresa/perfil" element={
-        <ProtectedRoute role="empresa">
-          <PerfilEmpresa />
-        </ProtectedRoute>
-      } />
+        {/* Rutas protegidas CLIENTE */}
+        <Route path="cliente/dashboard" element={
+          <ProtectedRoute role="cliente">
+            <ClienteDashboard />
+          </ProtectedRoute>
+        } />
 
-      <Route path="/empresa/productos" element={
-        <ProtectedRoute role="empresa">
-          <ProductosEmpresa />
-        </ProtectedRoute>
-      } />
+        {/* Rutas protegidas EMPRESA */}
+        <Route path="empresa/perfil" element={
+          <ProtectedRoute role="empresa">
+            <PerfilEmpresa />
+          </ProtectedRoute>
+        } />
+        <Route path="empresa/productos" element={
+          <ProtectedRoute role="empresa">
+            <ProductosEmpresa />
+          </ProtectedRoute>
+        } />
 
-      {/* Rutas protegidas ADMIN */}
-      <Route path="/admin" element={<AdminLayout />}>
-        <Route index element={<DashboardAdmin />} />
-        <Route path="dashboard" element={<DashboardAdmin />} />
-        <Route path="empresas" element={<EmpresasAdmin />} />
-        <Route path="clientes" element={<ClientesAdmin />} />
-        <Route path="administradores" element={<AdministradoresAdmin />} />
-        <Route path="administracion" element={<Administracion />} />
-        <Route path="clientes" element={<ProtectedRoute role="admin"><ListaClientes /></ProtectedRoute>} />
-        <Route path="clientes/nuevo" element={<ProtectedRoute role="admin"><FormularioCliente /></ProtectedRoute>} />
-        <Route path="clientes/editar/:id" element={<ProtectedRoute role="admin"><FormularioCliente /></ProtectedRoute>} />
+        {/* Rutas protegidas ADMIN */}
+        <Route path="admin/dashboard" element={
+          <ProtectedRoute role="admin">
+            <DashboardAdmin />
+          </ProtectedRoute>
+        } />
+        <Route path="admin/empresas" element={
+          <ProtectedRoute role="admin">
+            <EmpresasAdmin />
+          </ProtectedRoute>
+        } />
+        <Route path="admin/clientes" element={
+          <ProtectedRoute role="admin">
+            <ClientesAdmin />
+          </ProtectedRoute>
+        } />
+        <Route path="admin/administradores" element={
+          <ProtectedRoute role="admin">
+            <AdministradoresAdmin />
+          </ProtectedRoute>
+        } />
+        <Route path="admin/administracion" element={
+          <ProtectedRoute role="admin">
+            <Administracion />
+          </ProtectedRoute>
+        } />
+        <Route path="admin/clientes/lista" element={
+          <ProtectedRoute role="admin">
+            <ListaClientes />
+          </ProtectedRoute>
+        } />
+        <Route path="admin/clientes/nuevo" element={
+          <ProtectedRoute role="admin">
+            <FormularioCliente />
+          </ProtectedRoute>
+        } />
+        <Route path="admin/clientes/editar/:id" element={
+          <ProtectedRoute role="admin">
+            <FormularioCliente />
+          </ProtectedRoute>
+        } />
       </Route>
     </Routes>
   );
